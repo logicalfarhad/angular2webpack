1f49401a95a46466a90c4cdd26822832ec1554ab
logicalforhad <forhad.univdhaka@gmail.com>
2016-03-24 23:29:04 +0600

    npm version updated in package.json

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+///<reference path="../node_modules/angular2/typings/browser.d.ts"/>
 import {bootstrap} from 'angular2/platform/browser';
 import {SeedApp} from './app/components/heroList/seed-app';
 bootstrap(SeedApp).catch(err => console.error(err));
